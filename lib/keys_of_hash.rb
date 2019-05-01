8dd0357fe1219dd4f1a2b8965ccf7d9605f32c28
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-05-01 18:10:24 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,13 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*values)
+    matching_keys = []
+  values.each do |value|
+    self.each do |key1, value1|
+      if self[key1] == value
+        matching_keys << key1
+      end
+    end
   end
+  return matching_keys
+end
 end
